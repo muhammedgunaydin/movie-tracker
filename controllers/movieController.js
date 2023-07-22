aa4420b8637b3ee6f2743c9a92301fd866edfca8
muhammedgunaydin <muhammed-gunaydin@hotmail.com>
2023-07-23 00:03:20 +0300

    :sparkles: Add movie CRUD

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -4,9 +4,47 @@ const createMovie = async (req, res) => {
   try {
     const movie = await Movie.create(req.body)
     res.status(201).json(movie)
-  } catch(err) {
+  } catch (err) {
     res.status(400).send(`Error while creating movie : ${err.message}`)
   }
 }
 
-module.exports = {createMovie}
+const getAllMovies = async (req, res) => {
+  try {
+    const movies = await Movie.find()
+    res.status(200).json({ movies })
+  } catch (err) {
+    res.status(400).send(`Error while getting all movies : ${err.message}`)
+  }
+}
+
+const getMovieById = async (req, res) => {
+  try {
+    const { id } = req.params
+    const movie = await Movie.findOne({ _id: id })
+    res.status(200).json({ movie })
+  } catch (err) {
+    res.status(400).send(`Error while getting movie : ${err.message}`)
+  }
+}
+
+const updateMovie = async (req, res) => {
+  try {
+    const { id } = req.params
+    const movie = await Movie.findOneAndUpdate({ _id: id }, { $set: req.body })
+    res.status(200).send('Movie updated successfully')
+  } catch (err) {
+    res.status(400).send(`Error while updating movie : ${err.message}`)
+  }
+}
+
+const deleteMovie = async (req, res) => {
+  try {
+    const {id}=req.params
+    const movie = await Movie.findOneAndDelete({_id:id})
+    res.status(200).send('Movie deleted successfully')
+  } catch (err) {
+    res.status(400).send(`Error while deleting movie : ${err.message}`)
+  }
+}
+module.exports = { createMovie, getAllMovies, getMovieById, updateMovie,deleteMovie }
--- router/movieRouter.js
+++ router/movieRouter.js
@@ -4,5 +4,9 @@ const movieController = require('../controllers/movieController')
 const router = express.Router()
 
 router.post('/',movieController.createMovie)
+router.get('/',movieController.getAllMovies)
+router.get('/:id',movieController.getMovieById)
+router.patch('/:id',movieController.updateMovie)
+router.delete('/:id',movieController.deleteMovie)
 
 module.exports = router
